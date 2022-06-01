e3fb96cfb9a89bf23497f68e4b0ebc4a6c200b00
Gilberto Silva <contatogilbertolopes@outlook.com.br>
2022-06-01 08:55:39 -0300

    Update personalizacao.js

--- personalizacao.js
+++ personalizacao.js
@@ -1,1 +1,2 @@
-alert("Olá mundo!");
+var html_descricao = "<div class='form-group'><label>Opinião: </label><textarea id='campo_descricao' class='form-control' name='properties[Opinião]'></textarea></div>";
+$(html_descricao).insertAfter("#product_form > .js-product-variants");
